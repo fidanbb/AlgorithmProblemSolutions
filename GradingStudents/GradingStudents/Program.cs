01c8253316ebd7a175dcf60bfa1fbb392bb204ee
fidanbb <fidanbb@code.edu.az>
2024-05-26 23:05:30 +0400

    added GradingStudents HackerRankTask Solution

---
+++ GradingStudents/GradingStudents/Program.cs
@@ -0,0 +1,24 @@
+﻿
+static List<int> gradingStudents(List<int> grades)
+{
+
+
+    for (int i = 0; i < grades.Count; i++)
+    {
+        int nextMultiple = (((grades[i] / 5) + 1) * 5);
+
+        if ((nextMultiple - grades[i] < 3) && grades[i] >= 38)
+        {
+            grades[i] = nextMultiple;
+        }
+
+    }
+
+    return grades;
+}
+
+foreach (var item in gradingStudents(new List<int> { 73, 67, 40, 33 }))
+{
+    Console.WriteLine(item);
+}
+
