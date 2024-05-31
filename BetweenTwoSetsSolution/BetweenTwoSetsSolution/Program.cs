c26b098ba39f874231585e835e06e9978a744dfa
fidanbb <fidanbb@code.edu.az>
2024-05-31 22:35:39 +0400

    added betweenTwoSets hackerRankTask Solution

---
+++ BetweenTwoSetsSolution/BetweenTwoSetsSolution/Program.cs
@@ -0,0 +1,29 @@
+﻿static int GetTotalX(List<int> a, List<int> b)
+{
+    int maxA = a.Max();
+    int minB = b.Min();
+    int count = 0;
+
+    for (int x = maxA; x <= minB; x++)
+    {
+        
+        bool isMultipleOfAllA = a.All(elementA => x % elementA == 0);
+
+        
+        bool isFactorOfAllB = b.All(elementB => elementB % x == 0);
+
+        if (isMultipleOfAllA && isFactorOfAllB)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+
+List<int> a = new List<int> { 2, 4 };
+List<int> b = new List<int> { 16, 32, 96 };
+
+int total = GetTotalX(a, b);
+Console.WriteLine(total); 
