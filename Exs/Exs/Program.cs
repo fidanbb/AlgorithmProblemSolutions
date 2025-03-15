0838ff0384440ad51742bcce3736d86a51c17773
fidanbb <fidanbb@code.edu.az>
2025-03-15 21:06:13 +0400

    added new solutions

---
+++ Exs/Exs/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+
+#region Find the first non-repeating character in a string.
+
+char FirstNonRepeatingChar(string str)
+{
+    Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+    foreach (char c in str)
+    {
+        if (charCount.ContainsKey(c))
+            charCount[c]++;
+        else
+            charCount[c] = 1;
+    }
+
+    foreach (char c in str)
+    {
+        if (charCount[c] == 1)
+            return c;
+    }
+
+    return '\0';
+}
+
+Console.WriteLine(FirstNonRepeatingChar("swiss"));    // Output: 'w'
+Console.WriteLine(FirstNonRepeatingChar("racecar")); // Output: 'e'
+Console.WriteLine(FirstNonRepeatingChar("aabb"));   // Output: '\0'
+
+#endregion
+
+
+#region Find the Missing Number in an Array
+
+int FindMissingNum(List<int> list)
+{
+    int n = list.Count;
+    int expectedSum = (n * (n + 1) / 2);
+    int actualSum = list.Sum();
+
+    return expectedSum - actualSum;
+}
+
+
+Console.WriteLine(FindMissingNum(new List<int> { 9, 6, 4, 2, 3, 5, 7, 0, 1 }));
+Console.WriteLine(FindMissingNum(new List<int> { 3, 0, 1 }));
+
+
+#endregion
---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,48 @@
+﻿
+// if string is palindrome
+
+bool IsPalindrome(string str)
+{
+    string reversed = string.Empty;
+
+    char[] strArr = str.ToCharArray();
+
+    for (int i = strArr.Length - 1; i >= 0; i--)
+    {
+        reversed += strArr[i];
+    }
+
+    if (reversed == str)
+        return true;
+
+    return false;
+}
+
+Console.WriteLine(IsPalindrome("racecar"));
+Console.WriteLine(IsPalindrome("hello"));
+Console.WriteLine(IsPalindrome("level"));
+
+// if number is palindrome
+
+bool isNumberPalindrome(int num)
+{
+    int reversedNum = 0;
+    int original = num;
+
+    while (num > 0)
+    {
+        int digit = num % 10;
+        reversedNum = reversedNum * 10 + digit;
+        num = num / 10;
+    }
+
+    if (reversedNum == original)
+        return true;
+
+    return false;
+}
+
+Console.WriteLine(isNumberPalindrome(1221));
+Console.WriteLine(isNumberPalindrome(123321));
+Console.WriteLine(isNumberPalindrome(10));
+Console.WriteLine(isNumberPalindrome(123));
