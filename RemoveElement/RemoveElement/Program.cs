9f560aca6f0ef1afa5eb3b5b5d13a0b68d1d3ca5
fidanbb <fidanbb@code.edu.az>
2025-03-28 23:53:37 +0400

    added leet code RemoveElement Problem Solution

---
+++ RemoveElement/RemoveElement/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Remove element leet code problem solution
+ int RemoveElement(int[] nums, int val)
+{
+    int count = 0;
+    int n = nums.Length;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[count] = nums[i];
+            count++;
+        }
+    }
+    
+    for (int i = 0; i< n-count; i++)
+    {
+        Console.WriteLine(nums[i]);
+    }
+
+    return count;
+}
+
+Console.WriteLine(RemoveElement(new int []{3, 2, 2, 3 },3));
