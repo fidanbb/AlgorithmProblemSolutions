64840db0460e52f0a220440d79cdb143d6f81bc3
Fidan Bashirova <fidanbb@code.edu.az>
2025-08-31 21:05:26 +0400

    remove element leet code problem

---
+++ RemoveElementLeetCode/RemoveElementLeetCode/Program.cs
@@ -0,0 +1,23 @@
+﻿
+// remove element form array problem in leetcode
+
+
+int RemoveElement(int[] nums, int val)
+{
+
+    int k = 0;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[k] = nums[i];
+            k++;
+        } 
+    }
+
+    return k;
+}
+
+
+Console.WriteLine(RemoveElement(new int[] { 3, 2, 2, 3 }, 3));
