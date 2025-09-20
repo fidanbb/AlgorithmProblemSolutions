486d954adf9e3fc03ac33a557689edf5f0a3e6e4
fidanbb <fidanbb@code.edu.az>
2025-09-20 18:01:13 +0400

    added remove dublicates second leetCode problem

---
+++ RemoveDublicateLeetCode/RemoveDublicateLeetCode/Program.cs
@@ -0,0 +1,22 @@
+﻿
+static int RemoveDuplicates(int[] nums)
+{
+    if (nums == null) return 0;
+    if (nums.Length <= 2) return nums.Length;
+
+    int k = 2;
+
+    for (int i = 2; i< nums.Length; i++)
+    {
+        if (nums[i] != nums[k-2])
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+
+    return k;
+}
+
+Console.WriteLine(RemoveDuplicates(new int[] { 1, 1, 1, 2, 2, 3 }));
+Console.WriteLine(RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 }));
